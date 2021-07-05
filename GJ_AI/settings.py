93268cd29393ad5583b4d7ef3283bd6d5f151d43
JururuK <darma4545@gmail.com>
2021-07-05 22:08:06 +0900

    practice 2nd

--- GJ_AI/settings.py
+++ GJ_AI/settings.py
@@ -9,11 +9,23 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os, environ
 from pathlib import Path
+
+env = environ.Env(
+    #set casting. default value
+    DEBUG=(bool, False)
+)
+
+#reading .env file
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR, '.env')
+)
+# environ 이용하는 방법
 env_list = dict()
 local_env = open(os.path.join(BASE_DIR, '.env'))
 while True :
@@ -30,7 +42,7 @@ while True :
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7#7ccpc+006n4)l=ghwesd!39x!uu76r&9_=v+f*px9vc&a6$e'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
