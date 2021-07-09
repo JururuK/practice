c89d6b856006049b74bfb0eabb1b34bfe7c4c354
JururuK <darma4545@gmail.com>
2021-07-09 09:34:13 +0900

    7.9 class commit 2

--- myself/models.py
+++ myself/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+ #python manage.py makemigrations 하면 생성되는 0001_initial.py 파일은 변화를 추적해주므로 지우지 말것.
+class NewModel(models.Model) : #장고에서 기본적으로 제공하는 모델
+    text = models.CharField(max_length=255, null=False)
