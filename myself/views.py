0302b2c41ccdcb534a6cbbd6924bca9c89640e03
JururuK <darma4545@gmail.com>
2021-07-09 10:55:29 +0900

    7.9 class commit 4

--- myself/views.py
+++ myself/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def introduce(myself) :
-    if myself.method == "post": #추후에 경로를 특정하기 위해(헷갈림방지) myself(앱이름)폴더를 하나더 만들고 html파일을 넣어줌
+    if myself.method == "POST": #추후에 경로를 특정하기 위해(헷갈림방지) myself(앱이름)폴더를 하나더 만들고 html파일을 넣어줌
         return render(myself, 'myself/intro.html',
                       context={'text':'POST METHOD!'})
     else :
