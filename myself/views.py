9c90f55f7e60834d508795795c3d98beaccda4bd
JururuK <darma4545@gmail.com>
2021-07-13 09:20:30 +0900

    7.13 class commit 1

--- myself/views.py
+++ myself/views.py
@@ -2,10 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from myself.models import NewModel
+
+
 def introduce(myself) :
     if myself.method == "POST": #추후에 경로를 특정하기 위해(헷갈림방지) myself(앱이름)폴더를 하나더 만들고 html파일을 넣어줌
+
+        temp = myself.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save() #사용자가 입력한 값을 db에 저장
+
         return render(myself, 'myself/intro.html',
-                      context={'text':'POST METHOD!'})
+                      context={'model_instance': model_instance})
     else :
         return render(myself, 'myself/intro.html',
                       context={'text':'GET METHOD!'})
