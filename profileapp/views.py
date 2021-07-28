b8e85b1ceb35d7474c6b9bed45dfc0985f3c17a0
JururuK <darma4545@gmail.com>
2021-07-28 16:00:13 +0900

    7.28 class commit 3. profile id required

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 
 class ProfileCreateView(CreateView):
     model = Profile
@@ -14,10 +19,13 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('myself:introduce')
     template_name = 'profileapp/create.html'
 
+
     def form_valid(self,form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+@method_decorator(profile_ownership_required,'get')
+@method_decorator(profile_ownership_required,'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
